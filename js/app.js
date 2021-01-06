1e298d1a997abaa411f38b83911100b98de5ddf5
Collin Zeets <ckzeets@gmail.com>
2021-01-06 09:28:10 -0600

    Code review, updated JS and HTML

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 'use strict';
+const userFirstName = prompt('What is your name?');
+const userLastName = prompt('What is your last name?')
+const userName = (userFirstName + userLastName);
+alert('Hello ' + userFirstName + userLastName);
+
 const info = [
     { q : 'Is Canada my dream vacation?', a : false}, 
     { q : 'Is Summer my favorite Season?', a : false}, 
     { q : 'Is my name Collin?', a : true},
     { q : 'Is Sushi my favorite food?', a : true},
+    { q : 'Am I 22 years old?', a : true}
 ];
-function getBoolAnswer (question) {
+function getBoolAnswer(question) {
     while (true){
         const answer = prompt(question).toLowerCase();
         if (answer === 'yes' || answer === 'y' ) return true;
@@ -13,10 +19,5 @@ function getBoolAnswer (question) {
     }
 }
 for (let i = 0; i < info.length; i++){
-    console.log(getBoolAnswer(info[i].q))
-    
+    console.log(getBoolAnswer(info[i].q))  
 }
-const userFirstName = prompt('What is your name?');
-const userLastName = prompt('What is your last name?')
-const userName = (userFirstName + userLastName);
-alert('Hello ' + userFirstName + userLastName);
