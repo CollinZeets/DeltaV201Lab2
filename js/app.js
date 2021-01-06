897aabdc2539edf7cf22db9c3b22c54edd946eb3
Collin Zeets <ckzeets@gmail.com>
2021-01-05 20:27:47 -0600

    completed app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 'use strict';
+const info = [
+    { q : 'Is Canada my dream vacation?', a : false}, 
+    { q : 'Is Summer my favorite Season?', a : false}, 
+    { q : 'Is my name Collin?', a : true},
+    { q : 'Is Sushi my favorite food?', a : true},
+];
+function getBoolAnswer (question) {
+    while (true){
+        const answer = prompt(question).toLowerCase();
+        if (answer === 'yes' || answer === 'y' ) return true;
+        if (answer ==='no'|| answer === 'n') return false;
+    }
+}
+for (let i = 0; i < info.length; i++){
+    console.log(getBoolAnswer(info[i].q))
+    
+}
+const userFirstName = prompt('What is your name?');
+const userLastName = prompt('What is your last name?')
+const userName = (userFirstName + userLastName);
+alert('Hello ' + userFirstName + userLastName);
