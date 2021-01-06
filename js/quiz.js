56327eeb43556b6c521a4e24cf66aee36cf40228
Collin Zeets <ckzeets@gmail.com>
2021-01-06 10:07:38 -0600

    polishing quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -6,55 +6,61 @@ alert('Hello ' + userFirstName + userLastName);
 
 function theQuiz(){
     let points = 0;
-    let answerFirst prompt('Is canada my dream vacation?');
+    let answerFirst = prompt('Is canada my dream vacation?');
     answerFirst = answerFirst.toLowerCase();
-    if (answerfirst === 'n' || answerFirst ==='no')
+    if (answerfirst === 'n' || answerFirst ==='no'){
         alert ('Correct!');
         //console.log('Correct!')
-        points +=;
-    }else {
+        points ++;
+    }
+    else {
         alert('Incorrect...')
         //console.log('Incorrect...')
     }
 
-    let answerSecond prompt('Is summer my favorite season?');
+    let answerSecond = prompt('Is summer my favorite season?');
     answerSecond = answerSecond.toLowerCase();
-    if (answerSecond === 'n' || answerSecond ==='no')
-        alert ('Correct!');
-        //console.log('Correct!')
-        points +=;
-    }else {
-        alert('Incorrect...')
-        //console.log('Incorrect...')
-
-    let answerThird prompt('Is my name Collin?');
-        answerThird = answerThird.toLowerCase();
-    if (answerThird === 'y' || answerThird ==='yes')
-         alert ('Correct!');
-        //console.log('Correct!')
-            points +=;
-    }else {
-        alert('Incorrect...')
-        //console.log('Incorrect...')  
-
-    let answerFourth prompt('Is sushi my favorite food?');
-    answerFourth = answerFourth.toLowerCase();
-    if (answerfourth === 'n' || answerFourth ==='no')
+    if (answerSecond === 'n' || answerSecond ==='no'){
         alert ('Correct!');
         //console.log('Correct!')
         points ++;
-    }else {
+    }
+    else{ 
+        alert('Incorrect...')
+        //console.log('Incorrect...')
+    }
+    let answerThird = prompt('Is my name Collin?');
+        answerThird = answerThird.toLowerCase();
+    if (answerThird === 'y' || answerThird ==='yes'){
+         alert ('Correct!');
+        //console.log('Correct!')
+            points ++
+    }
+    else {
+        alert('Incorrect...')
+        //console.log('Incorrect...')  
+    }
+    let answerFourth = prompt('Is sushi my favorite food?');
+    answerFourth = answerFourth.toLowerCase();
+    if (answerfourth === 'n' || answerFourth ==='no'){
+        alert ('Correct!');
+        //console.log('Correct!')
+        points ++;
+    }
+    else {
         alert('Incorrect...')
         //console.log('Incorrect...') 
 
-        let answerFifth prompt('Am I 22 years old?');
+        let answerFifth = prompt('Am I 22 years old?');
         answerFifth = answerFifth.toLowerCase();
-        if (answerfifth === 'y' || answerFifth ==='yes')
+        if (answerfifth === 'y' || answerFifth ==='yes'){
             alert ('Correct!');
             //console.log('Correct!')
-            points ++;
+            points ++
         }else {
             alert('Incorrect...')
             //console.log('Incorrect...')
         }
-        alert 'You sucessfully got ' + points = "out of 5 points"
+        alert ("You sucessfully got " + points + "out of 5 points");
+    }
+}
